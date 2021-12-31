b8bbee7a24c9dd78687f10c4b14b0ee28ad7c3c5
flippia <79864573+flippia@users.noreply.github.com>
2021-12-31 10:10:58 -0500

    Update BeforeGrid.jsx

--- src/components/beforeGrid/BeforeGrid.jsx
+++ src/components/beforeGrid/BeforeGrid.jsx
@@ -4,7 +4,7 @@ const beforeGrid = ({ beforegrid,showContent,width }) => {
     return (
         <div className={styles['before-grid']}>
             <h3>Original:</h3>
-            <table style={{ width: `${600 / width}%` }}>
+            <table style={{ width: `${2.2 * width}rem` }}>
                 <tbody>
                     {beforegrid.map((row, rowIndex) => (
                         <tr key={rowIndex}>
